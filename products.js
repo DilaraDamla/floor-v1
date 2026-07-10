b476da7f38f6954cd2ad560f8057e404b3bbfc66
DilaraDamla <dilaradamlaakyol@gmail.com>
2026-07-10 22:18:41 +0300

    ürün detay sayfası düzenlendi

--- products.js
+++ products.js
@@ -12,6 +12,29 @@ description:"Kırmızı güllerden oluşan özel tasarım romantik buket."
 },
 
 {
+id:5,
+name:"Pembe Gül Buketi",
+price:990,
+category:"gul",
+image:"../images/gul.png",
+rating:5,
+stock:true,
+description:"Zarif pembe güllerle hazırlanmış romantik buket."
+},
+
+{
+id:6,
+name:"Beyaz Gül Buketi",
+price:980,
+category:"gul",
+image:"../images/gul.png",
+rating:5,
+stock:true,
+description:"Saflığı ve zarafeti yansıtan özel beyaz gül buketi."
+},
+
+
+{
 id:2,
 name:"Lale Buketi",
 price:850,
@@ -21,6 +44,29 @@ rating:5,
 stock:true,
 description:"Rengarenk lalelerden hazırlanan bahar buketi."
 },
+
+{
+id:7,
+name:"Renkli Laleler",
+price:920,
+category:"lale",
+image:"../images/lale.png",
+rating:5,
+stock:true,
+description:"Canlı renkleriyle hazırlanmış özel lale koleksiyonu."
+},
+
+{
+id:8,
+name:"Beyaz Laleler",
+price:900,
+category:"lale",
+image:"../images/lale.png",
+rating:5,
+stock:true,
+description:"Minimal ve zarif beyaz lale buketi."
+},
+
 
 {
 id:3,
@@ -34,6 +80,29 @@ description:"Şık ve zarif beyaz orkide."
 },
 
 {
+id:9,
+name:"Mor Orkide",
+price:1350,
+category:"orkide",
+image:"../images/orkide.png",
+rating:5,
+stock:true,
+description:"Özel tasarım mor orkide aranjmanı."
+},
+
+{
+id:10,
+name:"Mini Orkide",
+price:990,
+category:"orkide",
+image:"../images/orkide.png",
+rating:5,
+stock:true,
+description:"Küçük ve zarif mini orkide."
+},
+
+
+{
 id:4,
 name:"Papatya Buketi",
 price:780,
@@ -42,6 +111,17 @@ image:"../images/papatya.png",
 rating:5,
 stock:true,
 description:"Doğal kır papatyalarından hazırlanmıştır."
+},
+
+{
+id:11,
+name:"Renkli Papatyalar",
+price:820,
+category:"papatya",
+image:"../images/papatya.png",
+rating:5,
+stock:true,
+description:"Renkli papatyalarla hazırlanmış neşeli buket."
 }
 
 ];
--- script.js
+++ script.js
@@ -302,3 +302,27 @@ if (title) {
     }
 
 }
+let quantity = 1;
+
+
+function increaseQuantity(){
+
+    quantity++;
+
+    document.getElementById("quantity").textContent = quantity;
+
+}
+
+
+
+function decreaseQuantity(){
+
+    if(quantity > 1){
+
+        quantity--;
+
+    }
+
+    document.getElementById("quantity").textContent = quantity;
+
+}
